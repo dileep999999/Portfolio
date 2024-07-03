f66bddb1cad4e42fc81369f85336159b6990c9d1
Dileep <dileepkumarreddy9797@gmail.com>
2024-07-02 21:48:51 -0400

    installing packages and creating dummy pages

--- dileep/src/App.js
+++ dileep/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import About from './containers/about';
+import Home from './containers/home';
+import Resume from './containers/resume';
+import Skills from './containers/skills';
+import Portfolio from './containers/portfolio';
+import Contact from './containers/contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route index path='/' element= {<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/resume' element={<Resume/>}/>
+        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   );
 }
