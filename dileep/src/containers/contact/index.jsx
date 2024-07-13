a886d66ecdbcc2e0ed3f2f6466cd53d77c0d7ed7
Dileep <dileepkumarreddy9797@gmail.com>
2024-07-12 22:13:43 -0400

    contact and portfolio changes

--- dileep/src/containers/contact/index.jsx
+++ dileep/src/containers/contact/index.jsx
@@ -1,14 +1,108 @@
-import React from 'react';
-import {BsInfoCircle} from 'react-icons/bs';
+import React, { useState } from "react";
 import PageHeaderContent from '../../components/page-header-content';
+import { BsInfoCircleFill } from "react-icons/bs";
+import { Animate } from "react-simple-animate";
+import emailjs from 'emailjs-com';
+import "./styles.scss";
 
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    description: ''
+  });
 
-const Contact=()=>{
-    return(
-        <section id="contact" className="contact">
-        <PageHeaderContent  headerText ="My Contact" icon={<BsInfoCircle size={40}/>}/>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    emailjs.send('service_l7ixwlh', 'template_dg9lmmq', formData,'DtMOlAluR5sfpE3ga')
+      .then((response) => {
+        alert('Email sent successfully');
+        setFormData({
+            name: '',
+            email: '',
+            description: ''
+          });
+      })
+      .catch((error) => {
+        console.error('Failed to send email', error);
+        alert('Failed to send email');
+      });
+  };
+
+  return (
+    <section id="contact" className="contact">
+      <PageHeaderContent
+        headerText="My Contact"
+        icon={<BsInfoCircleFill size={40} />}
+      />
+      <div className="contact__content">
+        <Animate
+          play
+          duration={1}
+          delay={0}
+          start={{ transform: "translateX(-200px)" }}
+          end={{ transform: "translateX(0px)" }}
+        >
+          <h3 className="contact__content__header-text">Let's Talk</h3>
+        </Animate>
+        <Animate
+          play
+          duration={1}
+          delay={0}
+          start={{ transform: "translateX(200px)" }}
+          end={{ transform: "translateX(0px)" }}
+        >
+          <form className="contact__content__form" onSubmit={handleSubmit}>
+            <div className="contact__content__form__controlswrapper">
+              <div>
+                <input
+                  required
+                  name="name"
+                  className="inputName"
+                  type="text"
+                  value={formData.name}
+                  onChange={handleChange}
+                />
+                <label htmlFor="name" className="nameLabel">Name</label>
+              </div>
+              <div>
+                <input
+                  required
+                  name="email"
+                  className="inputEmail"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <label htmlFor="email" className="emailLabel">Email</label>
+              </div>
+              <div>
+                <textarea
+                  required
+                  name="description"
+                  className="inputDescription"
+                  rows="5"
+                  value={formData.description}
+                  onChange={handleChange}
+                />
+                <label htmlFor="description" className="descriptionLabel">Description</label>
+              </div>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </Animate>
+      </div>
     </section>
-    )
-}
+  );
+};
 
 export default Contact;
